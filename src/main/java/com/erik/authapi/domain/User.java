592e5f9f5212c60855f94384882740c21df6707d
ErikOliveiraa <erikg.oliveira25@gmail.com>
2026-05-12 11:08:46 -0300

    add user entity and user enum roles

---
+++ src/main/java/com/erik/authapi/domain/User.java
@@ -0,0 +1,85 @@
+package com.erik.authapi.domain;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import org.springframework.data.annotation.Id;
+
+import com.erik.authapi.domain.enums.Role;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String email;
+	private String password;
+	private Role role;
+	
+	public User() {
+	}
+
+	public User(Long id, String email, String password, Role role) {
+		this.id = id;
+		this.email = email;
+		this.password = password;
+		this.role = role;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Role getRole() {
+		return role;
+	}
+
+	public void setRole(Role role) {
+		this.role = role;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
+}
