d68daa27d410088f7d5e1a07f539e593c0276ed7
ErikOliveiraa <erikg.oliveira25@gmail.com>
2026-05-20 11:43:06 -0300

    add user services and user object not found exception

--- src/main/java/com/erik/authapi/services/UserService.java
+++ src/main/java/com/erik/authapi/services/UserService.java
@@ -1,5 +1,47 @@
 package com.erik.authapi.services;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.erik.authapi.domain.User;
+import com.erik.authapi.repository.UserRepository;
+import com.erik.authapi.services.exceptions.ObjectNotFoundException;
+
+@Service
 public class UserService {
 
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll(){
+		return repo.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User user) {
+		return repo.save(user);
+	}
+	
+	public void delete(Long id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+	
+	public User update(User obj) {
+		User newObj = findById(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+
+	private void updateData(User newObj, User obj) {
+		newObj.setEmail(obj.getEmail());
+		newObj.setPassword(obj.getPassword());
+	}
 }
